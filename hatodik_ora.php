1f17e91c0ca1b313e5be2857b286f0a138a9de14
simond-hue <achipowa205@gmail.com>
2019-10-11 07:40:18 +0200

    elso

---
+++ hatodik_ora.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		*{
+			background-color: #222222;
+			color: #FFFFFF;
+		}
+	</style>
+	<meta charset="UTF-8">
+</head>
+<body>
+</body>
+</html>
